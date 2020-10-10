328f803b9d18bb6c43c4076d509ac6a162db06b9
Alexis Guzmán <alegu@Alexiss-MacBook-Pro.local>
2020-10-10 03:57:54 -0500

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	s := make([]int, 0, n)
+
+	for i := 0; i < n; i++ {
+		var num int
+		fmt.Scan(&num)
+		s = append(s, num)
+	}
+
+	var sum int
+	for _, num := range s {
+		sum += num
+	}
+
+	fmt.Print(sum)
 }
